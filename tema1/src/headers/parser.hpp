02f16ca43c2e2af0623d26e6a005508483fa03da
Tiberiu Petre <tiberiu@localhost.localdomain>
2018-03-06 14:31:13 +0200

    Implementat citire partiala automat

--- tema1/src/headers/parser.hpp
+++ tema1/src/headers/parser.hpp
@@ -4,37 +4,39 @@
 #include <vector>
 #include <string.h>
 
-class Letter {
-    char* letter;
-public:
-    Letter() { this->letter = new char[1]; *(this->letter) = 'a'; }
-    Letter(char* letter) { this->letter = new char[strlen(letter)]; strcpy(this->letter, letter); }
-    ~Letter() { delete[] letter; }
-    char* getLetter() { return letter; }
-};
+namespace ParserClasses {
+    class Letter {
+        char* letter;
+    public:
+        Letter() { this->letter = new char[1]; *(this->letter) = 'a'; }
+        Letter(char* letter) { this->letter = new char[strlen(letter)+1]; strcpy(this->letter, letter); }
+        ~Letter() { delete[] letter; }
+        char* getLetter() { return letter; }
+    };
 
-class State {
-    int nrState;
-public:
-    State() { nrState = 0; }
-    State(int nrState) { this->nrState = nrState; }
-    int getState() { return nrState; }
-};
+    class State {
+        int nrState;
+    public:
+        State() { nrState = 0; }
+        State(int nrState) { this->nrState = nrState; }
+        int getState() { return nrState; }
+    };
 
-class Parser {
-    int nrStates; State *firstState;
-    std::vector<State> vStates, vFinalStates;
-    std::vector<Letter> vAlphabet;
-    std::vector< std::vector<Letter> > *transMatrix;
+    class Parser {
+        int nrStates; State *firstState;
+        std::vector<State> vStates, vFinalStates;
+        std::vector<Letter*> vAlphabet;
+        std::vector< std::vector<Letter> > *transMatrix;
 
-    void initTransMatrix(int nrStates);
-public:
-    Parser() { this->firstState = new State(); }
-    Parser(int intFirstState, int nrStates) { 
-        this->firstState = new State(intFirstState); 
-        this->nrStates = nrStates; initTransMatrix(this->nrStates);
-    }
-    Parser(State firstState) { this->firstState = new State(firstState); }
-    ~Parser() { delete firstState; }
-};
+        void initTransMatrix(int nrStates);
+    public:
+        Parser() { this->firstState = new State(); }
+        Parser(int intFirstState, int nrStates) { 
+            this->firstState = new State(intFirstState); 
+            this->nrStates = nrStates; initTransMatrix(this->nrStates);
+        }
+        Parser(State firstState) { this->firstState = new State(firstState); }
+        ~Parser() { delete firstState; }
+    };
+}
 #endif // LFA_TEMA1_PARSER_H_
--- tema1/src/headers/stdafx.hpp
+++ tema1/src/headers/stdafx.hpp
@@ -2,6 +2,15 @@
 #define LFA_TEMA1_STDAFX_H_
 
 #include <iostream>
+#include <fstream>
+//Working with strings
+#include <stdlib.h>
+#include <string.h>
+
 #include "parser.hpp"
+
+namespace myMainFunctions {
+    ParserClasses::Parser* getAutomata(std::ifstream& in);
+}
 
 #endif // LFA_TEMA1_STDAFX_H_
